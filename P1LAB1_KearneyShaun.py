1a7d272bb1af67fbf35817c236432a23bf6a4c9e
SKearne <kearneys4633@student.faytechcc.edu>
2025-06-12 19:46:23 -0400

    Add files via upload
    First Submission

---
+++ P1LAB1_KearneyShaun.py
@@ -0,0 +1,11 @@
+print("Shaun Kearney")
+print("June 11, 2025")
+print("P1LAB1")
+print("Creating Python Code using IDLE.")
+print("\n")
+
+
+first_name = input("Enter your first name:")
+last_name = input("Enter your last name:")
+
+print("Hello,", first_name,last_name +"!","Welcome to CTI-110")
