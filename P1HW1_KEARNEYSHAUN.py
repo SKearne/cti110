63f0f49646c08702986d2c5feb8275ce053f208d
SKearne <kearneys4633@student.faytechcc.edu>
2025-06-12 22:01:08 -0400

    Add files via upload

---
+++ P1HW1_KEARNEYSHAUN.py
@@ -0,0 +1,44 @@
+print("Shaun Kearney")
+print("June 12, 2025")
+print("P1HW1")
+print("Creating a calculating program")
+
+print("\n")
+
+print("-----Calculating Exponents----")
+
+print("\n")
+
+print("Enter an integer as the base value:",end="")
+base_value = int(input())
+
+print("\n")
+
+print("Enter an integer as the exponent:",end="")
+exponent = int(input())
+
+print("\n")
+print("\n")
+
+print(base_value,"raised to the power of",exponent,"is",base_value * base_value * base_value, "!!")
+
+print("\n")
+print("\n")
+
+print("-----Addition and Subtraction----")
+
+print("\n")
+print("\n")
+
+print("Enter a starting integer:",end ="")
+starting_integer = int(input())
+print("Enter an integer to add:",end ="")
+add = int(input())
+print("Enter an integer to subtract:",end ="")
+subtract = int(input())
+
+print("\n")
+print("\n")
+
+print(starting_integer,"+",add,"-",subtract,"is equal to",(starting_integer + add - subtract))
+
